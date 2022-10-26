2317070ba8ef2cae04105e1a65f2477135c34419
Przemysław Pliński <przemyslaw.plinski@itmagination.com>
2022-10-26 11:05:09 +0200

    check and radio

--- tests/DropCheckRadioPage.spec.js
+++ tests/DropCheckRadioPage.spec.js
@@ -1,12 +1,32 @@
 const {test, expect} = require('@playwright/test')
 const {POManager} = require('../pageobjects/POManager')
 
-test.only('Check All Dropdown Options', async ({page})=>
+test('Check All Dropdown Options', async ({page})=>
 {   
     const poManager = new POManager(page);
     const dropCheckRadioPage = poManager.getDropCheckRadioPage()
     await dropCheckRadioPage.goTo()
     await dropCheckRadioPage.verifyDropDowns()
+
+}
+)
+
+test('Checkbox', async ({page})=>
+{   
+    const poManager = new POManager(page);
+    const dropCheckRadioPage = poManager.getDropCheckRadioPage()
+    await dropCheckRadioPage.goTo()
+    await dropCheckRadioPage.verifyCheckBox()
+
+}
+)
+
+test('Radio', async ({page})=>
+{   
+    const poManager = new POManager(page);
+    const dropCheckRadioPage = poManager.getDropCheckRadioPage()
+    await dropCheckRadioPage.goTo()
+    await dropCheckRadioPage.verifyRadioButtons()
 
 }
 )
